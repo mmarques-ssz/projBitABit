f846f54d4909ca2e1afadbaae5bbb0814e421e10
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2020-12-18 20:55:51 -0300

    Add files via upload

---
+++ projBitABit/projBitABit/Bits.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projBitABit
+{
+    class Bits
+    {
+        private byte osBits;
+
+        public Bits()
+        {
+            this.osBits = 0;
+        }
+
+        public Bits(byte osBits)
+        {
+            this.osBits = osBits;
+        }
+
+        public void setBits(byte osBits)
+        {
+            this.osBits = osBits;
+        }
+
+        public byte getBits()
+        {
+            return this.osBits;
+        }
+
+        public bool getBits(int bit)
+        {
+            byte novoDado;
+            int valor;
+            valor = (int)Math.Pow(2, bit - 1);
+            novoDado = (byte)((int)this.osBits & valor);
+            return (novoDado > 0);
+        }
+    }
+}
---
+++ projBitABit/projBitABit/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace projBitABit
+{
+    public partial class Form1 : Form
+    {
+        private Bits bits;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bits = this.bits = new Bits();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bits.setBits(byte.Parse(textBox1.Text));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = Convert.ToString(bits.getBits());
+            label2.Text = Convert.ToString(bits.getBits(), 2);
+            label3.Text = Convert.ToString(bits.getBits(), 16).ToUpper();
+            lblBit1.Text = (bits.getBits(1)) ? "1" : "0";
+            lblBit2.Text = (bits.getBits(2)) ? "1" : "0";
+            lblBit3.Text = (bits.getBits(3)) ? "1" : "0";
+            lblBit4.Text = (bits.getBits(4)) ? "1" : "0";
+            lblBit5.Text = (bits.getBits(5)) ? "1" : "0";
+            lblBit6.Text = (bits.getBits(6)) ? "1" : "0";
+            lblBit7.Text = (bits.getBits(7)) ? "1" : "0";
+            lblBit8.Text = (bits.getBits(8)) ? "1" : "0";
+        }
+    }
+}
